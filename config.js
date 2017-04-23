73b81368894d4c9b545041bb2c1934910bff3959
ZhaoJunHan <zhaojunhan@kashuo.com>
2017-04-23 16:31:35 +0800

    ~~~~~~~

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-window.baseUrl='http://localhost:63342/frpgrate/';
+// window.baseUrl='http://localhost:3000/';
+window.baseUrl='http://47.93.51.158/frpgrate/';
 window.origin='http://47.93.51.158/jfinal_cms/';
 require.config({
     baseUrl:baseUrl,
